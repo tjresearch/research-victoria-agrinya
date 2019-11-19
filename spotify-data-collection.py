3ce39863f6800319603b94558dc4a1cfccbde033
victoria-agrinya <57760011+victoria-agrinya@users.noreply.github.com>
2019-11-19 10:16:33 -0500

    Imported Track class from resources module

--- spotify-data-collection.py
+++ spotify-data-collection.py
@@ -6,6 +6,7 @@ import spotipy
 import librosa
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+from resources import Track
 
 #token = util.prompt_for_user_token("aurumlibro", "playlist-read-private", "8f408e92f7d24929ae7ac2613ebc11dc", "59dcf725c5494f7a8b31672ff4d46655", "https://www.spotify.com/us/")
 manager = SpotifyClientCredentials("8f408e92f7d24929ae7ac2613ebc11dc", "59dcf725c5494f7a8b31672ff4d46655")
@@ -33,42 +34,6 @@ play = spo.user_playlist("spotify:user:billboard.com", "spotify:playlist:6UeSaky
 #             # outfile.write(spo.track(uri)["name"] + " by " + spo.track(uri)['album']['artists'][0]["name"])
 #             outfile.write("\n")
 
-
-class Track:
-    manager = SpotifyClientCredentials("8f408e92f7d24929ae7ac2613ebc11dc", "59dcf725c5494f7a8b31672ff4d46655")
-    spo = spotipy.Spotify(client_credentials_manager = manager)
-    play = spo.user_playlist("spotify:user:billboard.com", "spotify:playlist:6UeSakyzhiEt4NB3UAd6NQ") 
-
-    def __init__(self, id):
-        self.uri = id
-        self.features = spo.audio_features([id])[0]
-
-    def getName(self):
-        return self.spo.track(self.uri)["name"]
-    def getDance(self):
-        return self.features["danceability"]
-    def getEnergy(self):
-        return self.features["energy"]
-    def getKey(self):
-        return self.features["key"]
-    def getLoudness(self):
-        return self.features["loudness"]
-    def getMode(self):
-        return self.features["mode"]
-    def getSpeechiness(self):
-        return str(self.features["speechiness"])
-    def getAcousticness(self):
-        return str(self.features["acousticness"])
-    def getInstrumentalness(self):
-        return str(self.features["instrumentalness"])
-    def getLiveness(self):
-        return str(self.features["liveness"])
-    def getValence(self):
-        return str(self.features["valence"])
-    def getTempo(self):
-        return str(self.features["tempo"])
-    def getPop(self):
-        return self.spo.track(self.uri)['popularity']
 
 li = ['6b2RcmUt1g9N9mQ3CbjX2Y', '0rKtyWc8bvkriBthvHKY8d', '454Epa1vrCGFddHXKyC1kb', '5KBARWIxeMGkvUax9VtVC9',
 '3g9OCNXY2aSbGRQGY17qVE']
