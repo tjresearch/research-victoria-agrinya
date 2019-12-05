310a039710dc778fd0dbc98f9fa697c49930c3eb
victoria-agrinya <57760011+victoria-agrinya@users.noreply.github.com>
2019-12-05 10:16:17 -0500

    Update keras-cnn-1.py

--- keras-cnn-1.py
+++ keras-cnn-1.py
@@ -1,33 +1,42 @@
 #Victoria Agrinya
-#Last update: 11.26.19
+#Last update: 12.5.19
 
 from __future__ import absolute_import, division, print_function
 import tensorflow as tf
-from tensorflow import keras
-from keras import layers
+from tensorflow import keras as k
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Reshape
+from keras.layers import Embedding
+from keras.layers import Conv1D, GlobalMaxPooling1D
 import numpy as np
-from resources import SongData
+from resources import SongData, Track
 import pandas as pd
 
 #dataframe data should be a list of lists, columns should be a list of strings, and dtype should be float
 
 # songs = pd.read_excel(io="/Users/vicki/Documents/Senior_Research/Song Feature Data.xlsx", usecols=str)
-x = [[-488.75186, 96, 110.56700, 0.34500, 0.68200],[-450.65536, 86, 88.96400, 0.28400, 0.47900], [-475.66257, 84, 82.33800, 0.28300, 0.28100]]
-uris = ['6b2RcmUt1g9N9mQ3CbjX2Y']
+#x_train, x_test = lists of mfcc values for each song; y_train, y_test = labels of popularity scores attributed to each list of mfcc data
+uris = []
+with open("Hot_100_uris.txt", "r") as infile:
+    for i in infile.readlines():
+        uris.append(str(i.strip()[14:]))
+
 cols = ['mfcc', 'pop', 'tempo', 'valence', 'energy']
 df = SongData(uris, cols)
-mfcc = df.mfcc(0)
-for i in mfcc:
-    print(i)
 
-model = tf.keras.Sequential()
-keras.layers.Conv1D(1, 1, strides=1, padding='valid', data_format='channels_last', dilation_rate=1, activation=None, 
-use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
-activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+mfcc = df.mfcc(uris)
+print(len(mfcc))
 
-model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(layers.Conv2D(64, (5, 5), activation='relu'))
-model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(1000, activation='relu'))
-model.add(layers.Dense(num_classes, activation='softmax'))
+
+# model = Sequential()
+# model.add(Reshape((1,0), input_shape=(1000,)))
+# model.add(Conv1D(100, 10, activation='relu', input_shape=(TIME_PERIODS, num_sensors)))
+# model.add(Conv1D(100, 10, activation='relu'))
+# model.add(MaxPooling1D(3))
+# model.add(Conv1D(160, 10, activation='relu'))
+# model.add(Conv1D(160, 10, activation='relu'))
+# model.add(GlobalAveragePooling1D())
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
+# print(model.summary())
