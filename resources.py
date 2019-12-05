3b0ab10dde90ba7d40ed71ae50a49f1d970d6188
victoria-agrinya <57760011+victoria-agrinya@users.noreply.github.com>
2019-12-05 10:15:36 -0500

    Added getArtist function to Track and added SongData class

--- resources.py
+++ resources.py
@@ -1,5 +1,5 @@
 #Victoria Agrinya
-#Last updated: 11.19.19
+#Last updated: 12.5.19
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
@@ -15,6 +15,8 @@ class Track:
         self.features = spo.audio_features([id])[0]
     def getName(self):
         return self.spo.track(self.uri)["name"]
+    def getArtist(self):
+        return self.spo.track(self.uri)['album']['artists'][0]['name']
     def getDance(self):
         return self.features["danceability"]
     def getEnergy(self):
@@ -40,9 +42,57 @@ class Track:
     def getPop(self):
         return self.spo.track(self.uri)['popularity']
 
-# class SongData:
-#     def __init__(self, uri):
-#         super().__init__()
-#         self.dict = {}
-#         self.df = pd.DataFrame()
+class SongData:
+    manager = SpotifyClientCredentials("8f408e92f7d24929ae7ac2613ebc11dc", "59dcf725c5494f7a8b31672ff4d46655")
+    spo = spotipy.Spotify(client_credentials_manager = manager) 
+       
+    def __init__(self, uris, features):
+        self.id = uris
+        self.data = []
+        self.df = pd.DataFrame()
+        self.cols = features
+        self.li = []
+        self.train = []
+        self.test = []
+
+    def makeDF(self):
+        for x in self.id:
+            track = Track(x)
+            features = self.spo.audio_features([x])[0]
+            li = []
+            for i in self.cols:
+                if i == "mfcc":
+                    try:
+                        y, sr = lib.load("/Users/vicki/Documents/Senior_Research/Hot 100 MP3s/" + track.getName()+ ".mp3")
+                        mfcc = lib.feature.mfcc(y=y, sr=sr)
+                        li.append(mfcc[0][0])
+                    except FileNotFoundError:
+                        print("Song not found")
+                elif i == "pop":
+                    li.append(self.spo.track(x)['popularity'])
+                else: 
+                    li.append(features[i])
+            self.data.append(li)
+        self.df = pd.DataFrame(data = self.data, columns = self.cols, dtype = float, index = [i+1 for i in range(len(self.id))])
+        return self.df
     
+    def mfcc(self, uris):
+        for i in uris:
+            track = Track(i)
+            y, sr = lib.load("/Users/vicki/Documents/Senior_Research/Hot 100 MP3s/" + track.getArtist() + " - " + track.getName()+ ".mp3", duration=30)
+            mfcc = lib.feature.mfcc(y=y,sr=sr, n_mfcc=5)
+            self.li.append(mfcc[0:1000:1])
+        return self.li
+
+    def train_test(self):
+        for i in range(50):
+            track = Track(self.id[i])
+            x_train, y_train = self.li[i], track.getPop() 
+            self.train.append((x_train, y_train))
+        
+        for i in range(50, 100, 1):
+            track = Track(self.id[i])
+            x_test, y_test = self.li[i], track.getPop()
+            self.test.append((x_test, y_test))
+
+        return {"train": self.train, "test": self.test}
